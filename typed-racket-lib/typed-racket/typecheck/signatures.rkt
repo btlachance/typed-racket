a2b8899773c7065a2baae7105201967075a91943
Brian Lachance <blachance@gmail.com>
2015-11-07 11:23:35 -0500

    Fix signatures for check-contract unit
    We actually return results, not types, from all of the contract
    typechecking functions. The initial contracts were just cargo-culted..

--- typed-racket-lib/typed-racket/typecheck/signatures.rkt
+++ typed-racket-lib/typed-racket/typecheck/signatures.rkt
@@ -31,10 +31,10 @@
 
 (define-signature check-contract^
   ([cond-contracted check-contract (identifier? syntax? . -> . void?)]
-   [cond-contracted tc-arrow-contract (syntax? . -> . Type/c)]
-   [cond-contracted tc-arrow-i-contract (syntax? . -> . Type/c)]
-   [cond-contracted tc-and/c (syntax? . -> . Type/c)]
-   [cond-contracted tc-or/c (syntax? . -> . Type/c)]))
+   [cond-contracted tc-arrow-contract (syntax? . -> . full-tc-results/c)]
+   [cond-contracted tc-arrow-i-contract (syntax? . -> . full-tc-results/c)]
+   [cond-contracted tc-and/c (syntax? . -> . full-tc-results/c)]
+   [cond-contracted tc-or/c (syntax? . -> . full-tc-results/c)]))
 
 (define-signature tc-if^
   ([cond-contracted tc/if-twoarm ((syntax? syntax? syntax?) ((or/c tc-results/c #f)) . ->* . full-tc-results/c)]))
