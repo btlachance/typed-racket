d86024a96c0aeab09acc934b8bc39e4a788f1975
Brian Lachance <blachance@gmail.com>
2016-08-15 10:05:34 -0400

    Remove redundant tests

--- typed-racket-test/succeed/contract-out-gt.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(module gt-5-pro typed/racket
-  (provide (contract-out
-            [gt-5 (>/c 5)]))
-  (: gt-5 Integer)
-  (define gt-5 6))
-(require 'gt-5-pro)
-gt-5
--- typed-racket-test/succeed/ctc-typed-prov-contract.rkt
+++ typed-racket-test/succeed/ctc-typed-prov-contract.rkt
@@ -1,15 +1,19 @@
 #lang typed/racket
-;; checks that a contract can go from typed->typed then be used as a contract
-(module server typed/racket
-  (provide (contract-out [pizza-order/c any/c]))
+(module spec typed/racket
+  ;; contract values can be exported from typed modules
+  (provide pizza-order/c)
   (define pizza-order/c (->/c (integer-in 12 20)
                               (or/c 'small 'medium 'large)
                               any/c)))
 (module store typed/racket
-  (require (submod ".." server))
+  (require (submod ".." spec))
   (provide
    (contract-out
+    ;; and then required and applied by typed clients
     [order-pizza pizza-order/c]))
   (: order-pizza (-> Integer (U 'small 'medium 'large) Boolean))
   (define (order-pizza price size)
     #t))
+(require 'store)
+(order-pizza 12 'small)
+
